cbe60b6c00d872f75d413906db064240b7fb633b
OscarZhou <zhouhongyu1989@gmail.com>
2018-07-06 12:18:03 +1200

    Remove runProxy to give more freedom to developers

--- server_config.go
+++ server_config.go
@@ -3,8 +3,6 @@ package proxy
 import (
 	"encoding/json"
 	"io/ioutil"
-
-	"github.com/gin-gonic/gin"
 )
 
 // ServerConfig representing an array of Server
@@ -31,27 +29,4 @@ func (sc *ServerConfig) loadFile(fileName string) error {
 		return err
 	}
 	return nil
-}
-
-// RunProxy runs routing and forwarding configuration
-func (sc *ServerConfig) RunProxy(r *gin.Engine) {
-	for _, server := range *sc {
-		switch server.Method {
-		case "GET":
-			r.GET(server.Path, server.ReverseProxy())
-			r.GET(server.Path+"/:id", server.ReverseProxy())
-		case "POST":
-			r.POST(server.Path, server.ReverseProxy())
-		case "PUT":
-			r.PUT(server.Path, server.ReverseProxy())
-		case "DELETE":
-			r.DELETE(server.Path, server.ReverseProxy())
-		case "PATCH":
-			r.PATCH(server.Path, server.ReverseProxy())
-		case "OPTIONS":
-			r.OPTIONS(server.Path, server.ReverseProxy())
-		case "HEAD":
-			r.HEAD(server.Path, server.ReverseProxy())
-		}
-	}
 }
